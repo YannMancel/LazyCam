22d042750d9112169f241097d0351d7e6d4dbace
yann_mancel <yann.mancel@hotmail.fr>
2021-04-13 16:54:35 +0200

    ✨ feat: update CameraController

--- lib/controllers/camera_controller.dart
+++ lib/controllers/camera_controller.dart
@@ -1,11 +1,28 @@
-part of 'controllers_link.dart';
+import 'package:camera/camera.dart' as camera_lib;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CameraController extends StateNotifier<CameraState> {
-  CameraController() : super(CameraState.initial()) {
+import '../models/camera_state.dart';
+import 'controllers_link.dart';
+
+abstract class _CameraController {
+  camera_lib.CameraController get controller;
+  void switchCamera();
+  Future<void> recordMovie();
+  Future<void> startImageStream();
+  Future<void> stopImageStream();
+}
+
+class CameraController extends StateNotifier<CameraState>
+    implements _CameraController {
+  CameraController({
+    required ImageController imageController,
+  })   : _imageController = imageController,
+        super(CameraState.initial()) {
     _initialize();
   }
 
-  camera.CameraController? _controller;
+  final ImageController _imageController;
+  camera_lib.CameraController? _controller;
 
   set _error(String message) {
     state = CameraState.error(
@@ -14,27 +31,18 @@ class CameraController extends StateNotifier<CameraState> {
     );
   }
 
-  camera.CameraController get controller {
-    if (_controller == null) {
-      _error = 'Error during initialisation.';
-      throw 'CameraController has not called initialize method before this getter.';
-    }
-    return _controller!;
-  }
-
   Future<void> _initialize() async {
     state = CameraState.setup(isFirstCamera: state.isFirstCamera);
 
-    final cameras = await camera.availableCameras();
+    final cameras = await camera_lib.availableCameras();
 
     if (cameras.length == 1 && state.isFirstCamera == false) {
       _error = 'There is only on camera.';
-      throw 'Error during initialization of camera.';
     }
 
-    _controller = camera.CameraController(
+    _controller = camera_lib.CameraController(
       state.isFirstCamera ? cameras.first : cameras.last,
-      camera.ResolutionPreset.high,
+      camera_lib.ResolutionPreset.high,
     );
 
     await _controller!.initialize();
@@ -42,13 +50,35 @@ class CameraController extends StateNotifier<CameraState> {
     state = CameraState.readyPreview(isFirstCamera: state.isFirstCamera);
   }
 
+  @override
+  camera_lib.CameraController get controller {
+    if (_controller == null) _error = 'Error during initialisation.';
+    return _controller!;
+  }
+
+  @override
   void switchCamera() {
     state = CameraState.switchCamera(isFirstCamera: !state.isFirstCamera);
     _initialize();
   }
 
-  Future<void> start() async {
+  @override
+  Future<void> recordMovie() async {
     // todo add record action
+  }
+
+  @override
+  Future<void> startImageStream() async {
+    _imageController.startTimer(timeInSecond: 15);
+    await _controller!.startImageStream((image) {
+      _imageController.addImage = image;
+    });
+  }
+
+  @override
+  Future<void> stopImageStream() async {
+    _imageController.stopImageStream();
+    await _controller!.stopImageStream();
   }
 
   @override
