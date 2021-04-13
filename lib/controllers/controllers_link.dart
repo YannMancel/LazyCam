c37fc3cf54f187bac6bc4bfe628a59adba36e7f0
yann_mancel <yann.mancel@hotmail.fr>
2021-04-13 15:44:49 +0200

    👌 fix: update linker of controllers

--- lib/controllers/controllers_link.dart
+++ lib/controllers/controllers_link.dart
@@ -1,11 +1,3 @@
-import 'dart:io';
-
-import 'package:camera/camera.dart' as camera;
-import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:path_provider/path_provider.dart';
-
-import '../models/camera_state.dart';
-
-part 'camera_controller.dart';
-part 'video_controller.dart';
+export 'camera_controller.dart';
+export 'video_controller.dart';
+export 'image_controller.dart';
