3124e827a4069ae9a4b1aac15817735789652e32
yann_mancel <yann.mancel@hotmail.fr>
2021-05-22 15:56:16 +0200

    ✨ feat: CircularProgressIndicator in TimerPage

--- lib/pages/timer_page.dart
+++ lib/pages/timer_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart' show HookWidget;
+import 'package:flutter_hooks/flutter_hooks.dart'
+    show HookWidget, useAnimationController;
+import 'package:flutter_riverpod/flutter_riverpod.dart' show BuildContextX;
 import 'package:hooks_riverpod/hooks_riverpod.dart' show useProvider;
 
 import '../providers.dart';
@@ -11,7 +13,10 @@ class TimerPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final timerState = useProvider(timerProvider);
-    final timerController = useProvider(timerProvider.notifier);
+
+    final animationController = useAnimationController(
+      duration: const Duration(milliseconds: 200),
+    );
 
     return Scaffold(
       appBar: AppBar(
@@ -20,21 +25,63 @@ class TimerPage extends HookWidget {
       body: Center(
         child: timerState.when(
           initial: (_) => const StyledText(data: 'START'),
-          start: (seconds) => StyledText(data: '$seconds'),
+          start: (seconds) => _TimerView(seconds: seconds),
           stop: (_) => const StyledText(data: 'STOP'),
         ),
       ),
-      floatingActionButton: timerState.maybeWhen(
-        start: (_) => FloatingActionButton(
-          onPressed: timerController.stop,
-          child: const Icon(Icons.stop),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => timerState.maybeWhen(
+          start: (_) {
+            animationController.reverse();
+            context.read(timerProvider.notifier).stop();
+          },
+          orElse: () {
+            animationController.forward();
+            context.read(timerProvider.notifier).start(timeInSecond: 15);
+          },
         ),
-        orElse: () => FloatingActionButton(
-          onPressed: () => timerController.start(timeInSecond: 15),
-          child: const Icon(Icons.timer),
+        child: AnimatedIcon(
+          icon: AnimatedIcons.play_pause,
+          progress: animationController,
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class _TimerView extends StatelessWidget {
+  const _TimerView({
+    Key? key,
+    required int seconds,
+  })   : _seconds = seconds,
+        super(key: key);
+
+  final int _seconds;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              width: constraints.maxWidth * 0.7,
+              height: constraints.maxWidth * 0.7,
+              child: SizedBox.expand(
+                child: CircularProgressIndicator(
+                  value: ((15.0 - _seconds.toDouble()) * 1.0) / 15.0,
+                  backgroundColor: Colors.grey,
+                  valueColor: const AlwaysStoppedAnimation(Colors.red),
+                  strokeWidth: 15.0,
+                ),
+              ),
+            ),
+            StyledText(data: '$_seconds'),
+          ],
+        );
+      },
     );
   }
 }
